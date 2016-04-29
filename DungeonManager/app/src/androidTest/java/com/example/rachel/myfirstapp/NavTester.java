0d33e82954868d809403e3e436efec87ec56afd7
rochellev <rochellevroberts@gmail.com>
2016-04-28 21:46:12 -0500

    test update

--- DungeonManager/app/src/androidTest/java/com/example/rachel/myfirstapp/NavTester.java
+++ DungeonManager/app/src/androidTest/java/com/example/rachel/myfirstapp/NavTester.java
@@ -5,6 +5,7 @@ package com.example.rachel.myfirstapp;
  */
 
 import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.Direction;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.Until;
@@ -41,6 +42,21 @@ public class NavTester extends InstrumentationTestCase {
         UiObject2 resultText = device.findObject(By.clazz("android.widget.TextView"));
         boolean result = resultText.isEnabled();
         assertTrue(result);
+
+    }
+
+    public void testDMNavigation() throws Exception{
+        // Wait till picker buttons are on the screen
+        device.wait(Until.hasObject(By.text("Dungeon Master")), 3000);
+        // Select the dungeon master mode
+        UiObject2 masterSelectButton = device.findObject(By.text("Dungeon Master"));
+        masterSelectButton.click();
+        device.waitForIdle(3000);
+
+        UiObject2 swiper = device.findObject(By.clazz("android.widget.LinearLayout"));
+        swiper.swipe(Direction.RIGHT, 0.85f);
+        device.waitForIdle(7000);
+
 
     }
 
