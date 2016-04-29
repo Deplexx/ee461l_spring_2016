c080acd1e25c148ddb5e0bc6560466a08aa43bfd
Rachel Moor <rachelmoor@utexas.edu>
2016-04-28 21:50:27 -0500

    added more tests to UITesting

--- DungeonManager/app/src/androidTest/java/com/example/rachel/myfirstapp/UITesting.java
+++ DungeonManager/app/src/androidTest/java/com/example/rachel/myfirstapp/UITesting.java
@@ -1,19 +1,16 @@
 package com.example.rachel.myfirstapp;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
-import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.Collection;
 
@@ -24,31 +21,25 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
 import static android.support.test.runner.lifecycle.Stage.RESUMED;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class UITesting {
     Activity currentActivity;
-    private String mStringToBetyped;
 
     @Rule
     public ActivityTestRule<PickerActivity> mActivityRule = new ActivityTestRule<>(
             PickerActivity.class);
 
-    @Before
-    public void initValidString() {
-        // Specify a valid string.
-        mStringToBetyped = "Espresso";
-    }
-
     @Test
-    public void createCharacterTest() {
+    public void createNameTest() {
         // Type text and then press the button.
         onView(withId(R.id.buttonPlayer))
                 .perform(click());
         onView(withId(R.id.name_message)).perform(typeText("Aragorn"));
+        onView(withId(R.id.type_message)).perform(typeText("Ranger"));
+        onView(withId(R.id.class_message)).perform(typeText("Human"));
         closeSoftKeyboard();
         onView(withId(R.id.button)).perform(click());
         onView(withText(R.string.dialog_yes)).perform(click());
@@ -57,6 +48,8 @@ public class UITesting {
         CharacterSheet chac = CharacterSheet.loadSheet(settings);
 
         Assert.assertEquals(chac.name, "Aragorn");
+        Assert.assertEquals(chac.race, "Ranger");
+        Assert.assertEquals(chac.cclass, "Human");
     }
 
     public Activity getActivityInstance(){
--- DungeonManager/app/src/main/java/com/example/rachel/myfirstapp/DisplayMessageActivity.java
+++ DungeonManager/app/src/main/java/com/example/rachel/myfirstapp/DisplayMessageActivity.java
@@ -2,19 +2,13 @@ package com.example.rachel.myfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class DisplayMessageActivity extends AppCompatActivity {
