a4abc5b3572ed4dbe500edf383ae225aeaa682a8
William Glanton <wglanton77@gmail.com>
2016-05-05 00:28:35 -0500

    added saving dm character sheet

--- DungeonManager/app/src/main/java/com/example/rachel/myfirstapp/Logger_Display.java
+++ DungeonManager/app/src/main/java/com/example/rachel/myfirstapp/Logger_Display.java
@@ -15,14 +15,6 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- *
- * to handle interaction events.
- * create an instance of this fragment.
- */
 public class Logger_Display extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,38 +33,6 @@ public class Logger_Display extends Fragment {
 
     private CommandLogger logger;
 
-    //private SharedPreferences mPrefs;
-
-    //private View fragView;
-   // private OnFragmentInteractionListener mListener;
-    /*
-    public Logger_Display() {
-        // Required empty public constructor
-    }
-
-
-    // TODO: Rename and change types and number of parameters
-    public static Logger_Display newInstance(String param1, String param2) {
-        Logger_Display fragment = new Logger_Display();
-        Bundle args = new Bundle();
-
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
-    */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -141,45 +101,5 @@ public class Logger_Display extends Fragment {
         return view;
 
     }
-/*
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putSerializable("logger", logger);
-        outState.putString("PlayerName", Char_Name_Field.getText().toString());
-    }
 
-    /*
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    /*
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-    */
 }
